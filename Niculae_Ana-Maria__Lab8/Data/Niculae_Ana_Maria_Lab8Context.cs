bda8bfd5926d42744785d710392d8232abfd4861
ana11090 <72621705+ana11090@users.noreply.github.com>
2021-11-26 11:27:22 +0200

    Add files via upload

---
+++ Niculae_Ana-Maria__Lab8/Data/Niculae_Ana_Maria_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Niculae_Ana_Maria_Lab8.Models;
+
+namespace Niculae_Ana_Maria_Lab8.Data
+{
+    public class Niculae_Ana_Maria_Lab8Context : DbContext
+    {
+        public Niculae_Ana_Maria_Lab8Context (DbContextOptions<Niculae_Ana_Maria_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Niculae_Ana_Maria_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Niculae_Ana_Maria_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Niculae_Ana_Maria_Lab8.Models.Category> Category { get; set; }
+    }
+}
